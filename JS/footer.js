3b96eb2e28303649ec8cee783687669df9fe58c7
bcwu324 <bcwu324@gmail.com>
2020-09-06 17:19:03 +0800

    footer link done

--- JS/footer.js
+++ JS/footer.js
@@ -5,13 +5,17 @@ document.querySelector("footer").innerHTML = `
   <img class="footerfbImg" src="./images/logo/fb.png" alt="" />
 </div>
 <div class="footer footerLogo">
-  <img class="footerLogoImg" src="./images/logo/logo.png" alt="" />
+  <a href="https://www.facebook.com/PMphotomakeup/" target="new_blank"><img  src="./images/logo/logoPMbaby.png" /></a>
+  <a><img  src="./images/logo/logoMiddle.png" /></a>
+  <a href="https://www.facebook.com/PMstudiomakeup/" target="new_blank"><img  src="./images/logo/logoPMstudio.png" /></a>
 </div>
 <div class="footer footerIG">
   <img class="footerigImg" src="./images/logo/ig.png" alt="" />
 </div>
 <div class="footer footerLogo">
-  <img class="footerLogoImg" src="./images/logo/logo.png" alt="" />
+<a href="https://www.instagram.com/newbornpm/?hl=zh-tw" target="new_blank"><img  src="./images/logo/logoPMbaby.png" /></a>
+<a><img  src="./images/logo/logoMiddle.png" /></a>
+<a href="https://www.instagram.com/pmstudio68/?hl=zh-tw" target="new_blank"><img  src="./images/logo/logoPMstudio.png" /></a>
 </div>
 <div style="text-align:center;color:#8f6036;font-size:1.3rem;margin-bottom:3rem">
 台北市中山區錦西街1-3號3樓
